851ad136653a819cd1fdfd155c3ac22a0051fb1f
Nova <technobaboo@gmail.com>
2023-03-07 14:23:56 -0500

    main.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod pen;
+use pen::*;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    let (client, event_loop) = Client::connect_with_async_loop().await?;
+
+    let pen = Pen::new(client, PenSettings::default()?);
+    let _wrapped_root = client.wrap_root(pen)?;
+
+    tokio::select! {
+        _ = tokio::signal::ctrl_c() => (),
+        e = event_loop => e??,
+    }
+    Ok(())
+}
