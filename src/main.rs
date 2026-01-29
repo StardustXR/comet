188a40ee30a68bf539f63a25b1aeae96e48c57a2
Nova <technobaboo@gmail.com>
2026-01-29 01:06:52 -0800

    refactor: simplify dramatically

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::time::Instant;
-
 use glam::{Quat, Vec3};
 use stardust_xr_asteroids::{
+    CustomElement as _, Migrate, Reify,
     client::ClientState,
     elements::{Lines, Pen, PenState, Reparentable},
-    CustomElement as _, Migrate, Reify,
 };
 use stardust_xr_fusion::{
     drawable::{Line, LinePoint},
@@ -21,9 +19,7 @@ struct State {
     line_thickness: f32,
     pen_pos: Vec3,
     pen_rot: Quat,
-    last_pen_update_pos: Vec3,
-    #[serde(skip)]
-    grab_stopped: Option<Instant>,
+    prev_pos: Vec3,
 }
 impl Default for State {
     fn default() -> Self {
@@ -32,8 +28,7 @@ impl Default for State {
             line_thickness: 0.005,
             pen_pos: Vec3::ZERO,
             pen_rot: Quat::IDENTITY,
-            last_pen_update_pos: Vec3::ZERO,
-            grab_stopped: None,
+            prev_pos: Vec3::ZERO,
         }
     }
 }
@@ -51,52 +46,32 @@ impl Reify for State {
                 Pen::<State>::new(self.pen_pos, self.pen_rot, |state, pen_state, pos, rot| {
                     state.pen_pos = pos.into();
                     state.pen_rot = rot.into();
-                    let strength = match pen_state {
-                        PenState::StartedDrawing(v) => {
-                            if state.grab_stopped.is_none_or(|v| {
-                                Instant::now().duration_since(v).as_secs_f32() >= (1.0 / 30.0)
-                            }) {
-                                state.strokes.push(Line {
-                                    points: Vec::with_capacity(128),
-                                    cyclic: false,
-                                });
-                            }
-                            v
-                        }
-                        PenState::Drawing(v) => v,
-                        PenState::StoppedDrawing => {
-                            state.grab_stopped = Some(Instant::now());
-                            return;
-                        }
-                        PenState::Grabbed => return,
-                    };
-                    if state.last_pen_update_pos.distance(state.pen_pos) < 0.001 {
-                        return;
-                    }
-                    state.last_pen_update_pos = state.pen_pos;
-                    let Some(current_stroke) = state.strokes.last_mut() else {
-                        return;
-                    };
-                    current_stroke.points.push(LinePoint {
-                        point: pos,
-                        thickness: state.line_thickness * strength,
-                        color: rgba_linear!(1.0, 0.0, 0.0, 1.0),
-                    });
-                    if current_stroke.points.len() >= 128 {
-                        println!("making new line because of size");
-                        state.strokes.push(Line {
-                            points: {
-                                let mut vec = Vec::with_capacity(128);
-                                vec.push(LinePoint {
+                    match pen_state {
+                        PenState::StartedDrawing(strength) => {
+                            state.prev_pos = state.pen_pos;
+                            state.strokes.push(Line {
+                                points: vec![LinePoint {
                                     point: pos,
                                     thickness: state.line_thickness * strength,
                                     color: rgba_linear!(1.0, 0.0, 0.0, 1.0),
-                                });
-                                vec
-                            },
-                            cyclic: false,
-                        });
-                    }
+                                }],
+                                cyclic: false,
+                            });
+                        }
+                        PenState::Drawing(strength) => {
+                            if state.pen_pos.distance(state.prev_pos) > 0.001 {
+                                state.prev_pos = state.pen_pos;
+                                if let Some(current_stroke) = state.strokes.last_mut() {
+                                    current_stroke.points.push(LinePoint {
+                                        point: pos,
+                                        thickness: state.line_thickness * strength,
+                                        color: rgba_linear!(1.0, 0.0, 0.0, 1.0),
+                                    });
+                                }
+                            }
+                        }
+                        _ => {}
+                    };
                 })
                 .color(rgba_linear!(0.5, 0.0, 0.0, 1.0))
                 .hand_draw_threshold(0.65)
